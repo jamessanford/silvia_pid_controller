e33f856be455060b4931f420f9bf8997690a00e1
James Sanford <jsanford@froop.com>
2012-03-22 23:34:05 -0700

    Separate FakeLCD into its own file.

---
+++ FakeLCD.cpp
@@ -0,0 +1,54 @@
+/*
+ * An API similar to an LCD that dumps to the serial port.
+ * Copyright 2012 James Sanford and Dan Lange.
+ * All rights reserved.
+ *
+ * Use, modification, and redistribution of this software is permitted
+ * provided that this notice is retained with the software.
+ * This software is without warranty.
+ */
+
+#include "FakeLCD.h"
+
+FakeLCD::FakeLCD(int w, int h) {
+  width = min(LCD_WIDTH_MAX, w);
+  height = min(LCD_HEIGHT_MAX, h);
+  cursorX = 0;
+  cursorY = 0;
+  clear();
+}
+
+void FakeLCD::setCursor(int x, int y) {
+  cursorX = max(0, min(width - 1, x));
+  cursorY = max(0, min(height - 1, y));
+}
+
+void FakeLCD::show(void) {
+  int x;
+  int y;
+  Serial.print(F("* \r\n* "));
+  for (y = 0; y < height; y++) {
+    for (x = 0; x < width; x++) {
+      Serial.print((char)paneldata[(y * width) + x]);
+    }
+    Serial.print(F(" *\r\n* "));
+  }
+  Serial.print(F("\r\n\r\n"));
+}
+
+void FakeLCD::clear(void) {
+  cursorX = 0;
+  cursorY = 0;
+  int offset;
+  for (offset = 0; offset < (width * height); offset++) {
+    paneldata[offset] = 32;  // space, 0x20
+  }
+}
+
+size_t FakeLCD::write(uint8_t value) {
+  paneldata[(cursorY * width) + cursorX] = value;
+  cursorX += 1;
+  cursorX = min(width - 1, cursorX);
+  // no wrapping to next line
+  return 1;
+}
